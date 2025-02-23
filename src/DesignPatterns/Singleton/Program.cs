808410f9358221b118da390baae75120d9348511
falfang <f.thlover398@gmail.com>
2025-02-23 21:24:14 +0900

    Add Singleton

---
+++ src/DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using SingletonSample;
+
+Console.WriteLine("Start");
+
+Singleton obj1 = Singleton.GetSingleton();
+Singleton obj2 = Singleton.GetSingleton();
+
+if (obj1 == obj2)
+{
+    Console.WriteLine("obj1 is the same as obj2");
+}
+else
+{
+    Console.WriteLine("obj1 is not same as obj2");
+}
+
+Console.WriteLine("End");
